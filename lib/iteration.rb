d51203b1f927669e2997eb6db692775922cd119a
Tyree Williams <tyreewilliams515@gmail.com>
2019-11-20 22:55:14 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,4 +1,15 @@
 def join_ingredients(src)
+  row_index = 0
+  join_ingredients = []
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+      element_index += 1
+    end
+    join_ingredients << "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
+    row_index += 1
+  end
+  return join_ingredients
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
   # Build a new Array that contains strings where each pair of foods is
